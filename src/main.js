ed767deb4650c418858f92d4e9e2cd2f9ad15507
Kelly Ann Marshall <kelly@kellyannmarshall.com>
2020-09-23 06:19:09 +0100

    Remove unused imports

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Pivot from "vue-flexmonster";
-import "flexmonster/flexmonster.css";
-Vue.use(Pivot);
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 Vue.config.productionTip = false;
